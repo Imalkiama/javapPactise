d351d7e2701997b264b067dda2fd491e8ff3ace1
Imalkiama <85057789+Imalkiama@users.noreply.github.com>
2021-05-31 09:34:41 +0530

    Add files via upload

---
+++ Myname.java
@@ -0,0 +1,12 @@
+class Myname{
+	public static void main(String[]args){
+	System.out.println("1.Imalki Amanda,I am in first semester of BIT degree program");
+	System.out.println("1.Imalki Amanda");
+	System.out.println("2.I am in first semester of BIT degree program");
+	
+	
+	}
+
+
+
+}
